d7304ac90a642ea1ddb73e547e19f994898f82c4
Ivan Lapshin <tihdizer@gmail.com>
2025-11-23 17:31:37 +0300

    fix

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func main() {
 
 		// Проверка сети
 		netUsage := bodyFloats[6] / bodyFloats[5] * 100
-		if netUsage > 90 {
+		if netUsage >= 90 {
 			freeMbit := int(bodyFloats[5]-bodyFloats[6]) / (1000 * 1000)
 			fmt.Printf("Network bandwidth usage high: %d Mbit/s available\n", freeMbit)
 		}
